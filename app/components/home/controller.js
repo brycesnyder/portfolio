2df0a8deb99c2139df80ece9606969299d05e10a
Bryce Snyder <bryce.snyder@hotmail.com>
2015-12-14 12:17:05 -0500

    Changes to animations, adding new ones for gradient and background

--- app/components/home/controller.js
+++ app/components/home/controller.js
@@ -1,5 +1,5 @@
 app.controller('main', ['$scope', 'userData', function($scope, userData) {
   userData.success(function(data) {
-    $scope.data = data; // name, title, description, icons
+    $scope.data = data;
   });
 }]);
